f41b4ee5f643204782c9741b84c3f38e19040f47
djackson29 <danika.c.jackson@gmail.com>
2022-01-16 12:32:57 -0800

    doubles and cube root completed

--- lesson01/assignment1/es6.js
+++ lesson01/assignment1/es6.js
@@ -1,3 +1,66 @@
+// Doubles form
+const doublesForm = document.querySelector('#doubles')
+
+doublesForm.addEventListener('submit', function(event){
+	event.preventDefault()
+
+// Handle doubles input
+  const userInput = document.querySelector('#doubles .input').value
+  const userNumber = parseInt(userInput)
+  console.log(userNumber)
+
+// Handle doubles output
+  if(!isNaN(userNumber)){
+		const text = document.createTextNode(`${userNumber} * 2 = ${userNumber * 2}`)
+		const p = document.createElement('p')
+		p.appendChild(text)
+		this.appendChild(p)
+
+}});
+
+
+//Cube root form
+const cubeForm = document.querySelector('#cubeRoot')
+
+cubeForm.addEventListener('submit', function(event){
+	event.preventDefault()
+
+// Handle cube root input
+  const userInput = document.querySelector('#cubeRoot .input').value
+  const userNumber = parseInt(userInput)
+  console.log(userNumber)
+
+// Handle cube root output
+  if(!isNaN(userNumber)){
+		document.getElementById("cubeOutput").innerHTML = "The cube root of " + userNumber + " is " + Math.cbrt(userNumber);
+
+}});
+
+
+//Luck form
+const luckForm = document.querySelector('#luck')
+
+luckForm.addEventListener('submit', function(event){
+	event.preventDefault()
+
+// Handle luck input
+  const userInput = document.querySelector('#luck .input').value
+  const userNumber = parseInt(userInput)
+  console.log(userNumber)
+
+// Handle luck output
+  if(!isNaN(userNumber)){
+		let luckyNumbers = [7, 8, 13, 29];
+		let luckyNumber = luckyNumbers.find(myFunction);
+		luckyNumber.includes(userNumber);
+
+		function myFunction(value) {
+			if (userNumber.value = luckyNumbers){
+				document.getElementById("luckOutput").innerHTML = luckyNumber + " is lucky.";
+			}
+}}});
+
+
 //Cats form
 const catForm = document.querySelector('#cats')
 
@@ -20,85 +83,4 @@ catForm.addEventListener('submit', function(event){
 		function myFunction(value, index, array) {
 		  return value < 4;
 		}
-
 }});
-
-
-//Dogs form
-const dogForm = document.querySelector('#dogs')
-
-dogForm.addEventListener('submit', function(event){
-	event.preventDefault()
-
-// Handle dogs input
-  const userInput = document.querySelector('#dogs .input').value
-  const userNumber = parseInt(userInput)
-  console.log(userNumber)
-
-// Handle dogs output
-  if(!isNaN(userNumber)){
-
-
-
-}});
-
-
-//Monkeys form
-const monkeyForm = document.querySelector('#monkeys')
-
-monkeyForm.addEventListener('submit', function(event){
-	event.preventDefault()
-
-// Handle monkeys input
-  const userInput = document.querySelector('#monkeys .input').value
-  const userNumber = parseInt(userInput)
-  console.log(userNumber)
-
-// Handle monkeys output
-  if(!isNaN(userNumber)){
-
-
-
-}});
-
-
-
-
-// Lions form
-const lionForm = document.querySelector('#lions')
-
-lionForm.addEventListener('submit', function(event){
-	event.preventDefault()
-
-// Handle lions input
-  const userInput = document.querySelector('#lions .input').value
-  const userNumber = parseInt(userInput)
-  console.log(userNumber)
-
-// Handle lions output
-  if(!isNaN(userNumber)){
-		const text = document.createTextNode(`${userNumber * 2} lions! Just kidding, ${userNumber} is the perfect amount!`)
-		const p = document.createElement('p')
-		p.appendChild(text)
-		this.appendChild(p)
-
-}});
-
-
-
-
-
-
-
-
-
-//const text = document.createTextNode(`${userNumber * moreCats} cats? Wow!`)
-//function myFunction(value, index, array) {
-//  txt += value + "<br>";
-
-
-//let idealCats = numbers.find(myFunction)
-//document.getElementById("demo").innerHTML = "a much more ideal number of cats is " + idealCats;
-//function myFunction(value){
-//	return value > 18;
-//}
