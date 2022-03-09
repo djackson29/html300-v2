208ba59aa7dca6b04322819198d566baf289470f
djackson29 <danika.c.jackson@gmail.com>
2022-03-08 21:22:32 -0800

    created component file for images

---
+++ lesson08/assignment/boxoffice/src/components/imagesDiscover.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="imagesDiscover">
+  <h1>{{ msg }}</h1>
+
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'imagesDiscover',
+  props: {
+    msg: String,
+    src: String,
+    alt: String,
+    title: String
+  },
+  data() {
+    return {
+
+      // 3 arrays, for images in each genre
+      actionPosters: [require('../assets/black-panther.jpg'), require('../assets/hunger-games.jpg'), require('../assets/zombieland.jpg')],
+      musicalPosters: [require('../assets/greatest-showman.jpg'), require('../assets/hamilton.jpeg'), require('../assets/west-side.jpg')],
+      thrillerPosters: [require('../assets/jaws.jpg'), require('../assets/joker.jpeg'), require('../assets/lambs.jpg')]
+    }
+  }
+}
+</script>
